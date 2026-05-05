7c6f875b60c0277a4e583adc396d87e1251a2417
omomo <om062006@gmail.com>
2026-05-05 21:52:40 +0530

    add loop to execute fetch, decode, execute in order

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,23 @@ mod cpu;
 mod decoder;
 mod executor;
 mod instructions;
+mod fetcher;
+
+use std::{fs::File, io::Read};
+use cpu::Cpu;
 
 fn main() {
-    println!("Hello, world!");
+    
+    let mut cpu = Cpu::new();
+    let mut file = File::open("binary").unwrap();
+    let mut binary: Vec<u8> = Vec::new();
+
+    file.read_to_end(&mut binary).unwrap();
+    cpu.load_binary(&binary);
+
+    loop {
+        let raw_inst = cpu.fetch();
+        let inst = cpu.decode(raw_inst);
+        cpu.execute(inst);
+    }
 }
