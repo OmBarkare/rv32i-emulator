2ea05f656d59d3f445b66d468418a8423b397185
omomo <om062006@gmail.com>
2026-05-05 21:51:50 +0530

    feat: implement new and load_binary for Cpu struct

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,3 +3,19 @@ pub struct Cpu {
     pub pc: u32,
     pub mem: Vec<u8>,
 }
+
+impl Cpu {
+    pub fn new() -> Cpu {
+        Cpu {
+            regs: [0u32; 32],
+            pc: 0u32,
+            mem: Vec::new(),
+        }
+    }
+
+    pub fn load_binary(&mut self, binary: &[u8]) {
+        for (i, byte) in binary.iter().enumerate() {
+            self.mem[i] = *byte;
+        }
+    }
+}
