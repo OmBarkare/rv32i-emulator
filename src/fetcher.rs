d657759ebececc963ad8aa1a8527f3a7f48ab0db
omomo <om062006@gmail.com>
2026-05-06 10:48:46 +0530

    save current_pc before incrementing pc

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -1,10 +1,6 @@
 use crate::cpu::Cpu;
 use crate::instructions::RawInstruction;
 
-// TODO: pc shuold update in fetch instead of execute
-// Then we can also remove the akward (-4) everwhere in execute where 
-// PC is updated in the branch instructions
-
 impl Cpu {
     pub fn fetch(&mut self) -> RawInstruction {
         let mut inst: u32 = 0;
@@ -13,6 +9,7 @@ impl Cpu {
             curr_byte = (self.mem[self.pc as usize + i] as u32) << 8*i;
             inst |= curr_byte;
         }
+        self.curr_pc = self.pc;
         self.pc += 4;
         RawInstruction { bits:inst }
     }
