6e77312b5caae36ad69948824c4513c7dc9ed735
Om Barkare <ombarkare123@gmail.com>
2026-05-14 21:35:17 +0530

    src/main: implement word and half word read/writes
    to read integers or instructions from memory without having to
    read the whole page.
    This is not paging that the operating system implements, this is
    similar "lazy allocation" technique so that I don't have to allocate
    whole virtual address space worth for a process.

--- src/memory.rs
+++ src/memory.rs
@@ -8,21 +8,21 @@ impl Memory {
         Memory { pages: vec![None; 1 << 20] }
     }
 
-    pub fn allocate_page(&mut self, addr: u32, num: u16) -> Result<(), ()> {
+    pub fn allocate_page(&mut self, v_addr: u32, num: u16) -> Result<(), ()> {
         // addr is the full address, including the lower 12 bits
-        let v_addr = addr >> 12;
+        let p_addr = v_addr >> 12;
         for n in 0..num {
-            self.pages[(v_addr + n as u32) as usize] = Some( Box::from([0u8; 4096]) );
+            self.pages[(p_addr + n as u32) as usize] = Some( Box::from([0u8; 4096]) );
         }
 
         Ok(())
     }
 
     // read page from memory to buffer provided by buffer
-    pub fn read_page(&self, addr: u32, buf: &mut [u8; 4096]) -> Result<(), ()>{
-        let v_addr = addr >> 12;
+    pub fn read_page(&self, v_addr: u32, buf: &mut [u8; 4096]) -> Result<(), ()>{
+        let p_addr = v_addr >> 12;
 
-        if let Some(page) = &self.pages[v_addr as usize] {
+        if let Some(page) = &self.pages[p_addr as usize] {
             buf.copy_from_slice(&**page);
             Ok(())
         } else  {
@@ -31,10 +31,74 @@ impl Memory {
     }
 
     // write page from memory to buffer provided by buffer
-    pub fn write_page(&mut self, addr: u32, buf: &[u8; 4096]) -> Result<(), ()> {
-        let v_addr = addr >> 12;
-        self.pages[v_addr as usize] = Some( Box::new(*buf) );
+    pub fn write_page(&mut self, v_addr: u32, buf: &[u8; 4096]) -> Result<(), ()> {
+        let p_addr = v_addr >> 12;
+        self.pages[p_addr as usize] = Some( Box::new(*buf) );
 
         Ok(())
     }
+
+    // write half word as little endian
+    pub fn write_16(&mut self, v_addr: u32, hword: u16) -> Result<(), ()> {
+        let p_addr = v_addr >> 12;
+        let p_offset = v_addr & 0xFFF;
+
+        if let Some(page) = &mut self.pages[p_addr as usize] {
+            page[p_offset as usize] = hword as u8;
+            page[p_offset as usize + 1] = (hword >> 8) as u8;
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    // write word as little endian
+    pub fn write_32(&mut self, v_addr: u32, word: u32) -> Result<(), ()> {
+        let p_addr = v_addr >> 12;
+        let p_offset = v_addr & 0xFFF;
+
+        if let Some(page) = &mut self.pages[p_addr as usize] {
+            page[p_offset as usize] = word as u8;
+            page[p_offset as usize + 1] = (word >> 8) as u8;
+            page[p_offset as usize + 2] = (word >> 16) as u8;
+            page[p_offset as usize + 3] = (word >> 24) as u8;
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+
+    // read half word from little endian
+    pub fn read_16(&self, v_addr: u32) -> Result<u16, ()> {
+        let p_addr = v_addr >> 12;
+        let p_offset = v_addr & 0xFFF;
+
+        if let Some(page) = &self.pages[p_addr as usize] {
+            Ok(
+                (page[p_offset as usize] as u16)
+                | ((page[p_offset as usize + 1] as u16) << 8)
+            )
+        } else {
+            Err(())
+        }
+    }
+
+    // read word from little endian
+    pub fn read_32(&self, v_addr: u32) -> Result<u32, ()> {
+        let p_addr = v_addr >> 12;
+        let p_offset = v_addr & 0xFFF;
+
+        if let Some(page) = &self.pages[p_addr as usize] {
+            Ok(
+                (page[p_offset as usize] as u32)
+                | ((page[p_offset as usize + 1] as u32) << 8)
+                | ((page[p_offset as usize + 2] as u32) << 16)
+                | ((page[p_offset as usize + 3] as u32) << 24)
+            )
+        } else {
+            Err(())
+        }
+    }
+
 }
