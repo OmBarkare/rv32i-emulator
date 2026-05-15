3f247d13e45d891041e88abb206ca27278f4b452
Om Barkare <ombarkare123@gmail.com>
2026-05-15 11:22:44 +0530

    src/fetcher.rs: change to use new memory api

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -3,14 +3,8 @@ use crate::instructions::RawInstruction;
 
 impl Cpu {
     pub fn fetch(&mut self) -> RawInstruction {
-        let mut inst: u32 = 0;
-        let mut curr_byte: u32 = 0;
-        for i in 0..4 {
-            curr_byte = (self.mem[self.pc as usize + i] as u32) << 8*i;
-            inst |= curr_byte;
-        }
         self.curr_pc = self.pc;
         self.pc += 4;
-        RawInstruction { bits:inst }
+        RawInstruction { bits: self.mem.read_32(self.pc).unwrap() }
     }
 }
