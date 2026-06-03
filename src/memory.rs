d575ee5dce928eb338abb932194bd1c92ea71572
Om Barkare <ombarkare123@gmail.com>
2026-06-03 17:04:22 +0530

    src/memory.rs fix: allocate at the time of write
    instead of having to allocate and then use write to memory, improved
    to write directly, it allocates if memory is not already allocated
    this was done because of some problems with the current api. If you
    wanted to write to memory, you would first have to read it to check
    if it is allocated. If not, then read returns None, for which you
    would then allocate and then write.
    all write api's now check if the memory us already allocated
    and allocates if not already

--- src/memory.rs
+++ src/memory.rs
@@ -1,18 +1,20 @@
 pub struct Memory {
-    pages: Vec<Option<Box<[u8; 4096]>>>
+    pages: Vec<Option<Box<[u8; 4096]>>>,
 }
 
 impl Memory {
-    // new Memory struct with unallocated pages. 
+    // new Memory struct with unallocated pages.
     pub fn new() -> Self {
-        Memory { pages: vec![None; 1 << 20] }
+        Memory {
+            pages: vec![None; 1 << 20],
+        }
     }
 
     pub fn allocate_page(&mut self, v_addr: u32, num: u16) -> Result<(), ()> {
         // addr is the full address, including the lower 12 bits
         let p_addr = v_addr >> 12;
         for n in 0..num {
-            self.pages[(p_addr + n as u32) as usize] = Some( Box::from([0u8; 4096]) );
+            self.pages[(p_addr + n as u32) as usize] = Some(Box::from([0u8; 4096]));
         }
 
         Ok(())
@@ -21,7 +23,7 @@ impl Memory {
     // write page from memory to buffer provided by buffer
     pub fn write_page(&mut self, v_addr: u32, buf: &[u8; 4096]) -> Result<(), ()> {
         let p_addr = v_addr >> 12;
-        self.pages[p_addr as usize] = Some( Box::new(*buf) );
+        self.pages[p_addr as usize] = Some(Box::new(*buf));
 
         Ok(())
     }
@@ -30,11 +32,22 @@ impl Memory {
         let p_addr = v_addr >> 12;
         let p_offset = v_addr & 0xFFF;
 
-        if let Some(page) = &mut self.pages[p_addr as usize] {
-            page[p_offset as usize] = byte;
-            Ok(())
-        } else {
-            Err(())
+        match &mut self.pages[p_addr as usize] {
+            Some(page) => {
+                page[p_offset as usize] = byte;
+                Ok(())
+            }
+
+            None => {
+                self.allocate_page(v_addr, 1).unwrap();
+                if let Some(page) = &mut self.pages[p_addr as usize] {
+                    page[p_offset as usize] = byte;
+                } else {
+                    return Err(());
+                }
+
+                Ok(())
+            }
         }
     }
 
@@ -43,12 +56,23 @@ impl Memory {
         let p_addr = v_addr >> 12;
         let p_offset = v_addr & 0xFFF;
 
-        if let Some(page) = &mut self.pages[p_addr as usize] {
-            page[p_offset as usize] = hword as u8;
-            page[p_offset as usize + 1] = (hword >> 8) as u8;
-            Ok(())
-        } else {
-            Err(())
+        match &mut self.pages[p_addr as usize] {
+            Some(page) => {
+                page[p_offset as usize] = hword as u8;
+                page[p_offset as usize + 1] = (hword >> 8) as u8;
+                return Ok(());
+            }
+
+            None => {
+                self.allocate_page(v_addr, 1).unwrap();
+                if let Some(page) = &mut self.pages[p_addr as usize] {
+                    page[p_offset as usize] = hword as u8;
+                    page[(p_offset + 1) as usize] = (hword >> 8) as u8;
+                    return Ok(());
+                } else {
+                    return Err(());
+                }
+            }
         }
     }
 
@@ -69,18 +93,17 @@ impl Memory {
     }
 
     // read page from memory to buffer provided by buffer
-    pub fn read_page(&self, v_addr: u32, buf: &mut [u8; 4096]) -> Result<(), ()>{
+    pub fn read_page(&self, v_addr: u32, buf: &mut [u8; 4096]) -> Result<(), ()> {
         let p_addr = v_addr >> 12;
 
         if let Some(page) = &self.pages[p_addr as usize] {
             buf.copy_from_slice(&**page);
             Ok(())
-        } else  {
+        } else {
             Err(())
         }
     }
 
-    
     pub fn read_8(&self, v_addr: u32) -> Result<u8, ()> {
         let p_addr = v_addr >> 12;
         let p_offset = v_addr & 0xFFF;
@@ -92,17 +115,13 @@ impl Memory {
         }
     }
 
-
     // read half word from little endian
     pub fn read_16(&self, v_addr: u32) -> Result<u16, ()> {
         let p_addr = v_addr >> 12;
         let p_offset = v_addr & 0xFFF;
 
         if let Some(page) = &self.pages[p_addr as usize] {
-            Ok(
-                (page[p_offset as usize] as u16)
-                | ((page[p_offset as usize + 1] as u16) << 8)
-            )
+            Ok((page[p_offset as usize] as u16) | ((page[p_offset as usize + 1] as u16) << 8))
         } else {
             Err(())
         }
@@ -114,15 +133,13 @@ impl Memory {
         let p_offset = v_addr & 0xFFF;
 
         if let Some(page) = &self.pages[p_addr as usize] {
-            Ok(
-                (page[p_offset as usize] as u32)
+            Ok((page[p_offset as usize] as u32)
                 | ((page[p_offset as usize + 1] as u32) << 8)
                 | ((page[p_offset as usize + 2] as u32) << 16)
-                | ((page[p_offset as usize + 3] as u32) << 24)
-            )
+                | ((page[p_offset as usize + 3] as u32) << 24))
         } else {
             Err(())
         }
     }
-
 }
+
