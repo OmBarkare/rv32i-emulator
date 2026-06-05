547507725d321c9c33010c3197f142e9b93e1662
Om Barkare <ombarkare123@gmail.com>
2026-06-05 11:41:03 +0530

    src/cpu.rs add a Csrs struct to Cpu
    The base ISA is implemented, only the ECALL and EBREAK instructions
    remain. ECALL and EBREAK raise synchronous exceptions which need
    trap mechanisms to handle them. To provide this functionality,
    the M-mode CSR are needed.
    This struct will also be expanded upon when privilege modes are
    implemented, starting with the M mode

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,9 +1,35 @@
 use crate::memory::Memory;
+pub struct Csrs {
+    pub mtvec: u32,    // trap vector base address
+    pub mepc: u32,     // machine exception PC
+    pub mcause: u32,   // machine exception cause
+    pub mtval: u32,    // machine trap value
+    pub mstatus: u32,  // machine status register
+    pub mie: u32,      // machine interrupt enable
+    pub mip: u32,      // machine interrupt pending
+    pub mscratch: u32, // temparary stratch word
+}
+
+impl Csrs {
+    fn init() -> Self {
+        Csrs {
+            mtvec: 0,
+            mepc: 0,
+            mcause: 0,
+            mtval: 0,
+            mstatus: 0,
+            mie: 0,
+            mip: 0,
+            mscratch: 0,
+        }
+    }
+}
 pub struct Cpu {
     pub regs: [u32; 32],
     pub pc: u32, // stores address of instruction to be fetched, which will be executed next
     pub mem: Memory,
     pub curr_pc: u32, // stores address of instruction being executed
+    pub csrs: Csrs,
 }
 
 impl Cpu {
@@ -13,6 +39,7 @@ impl Cpu {
             pc: 0u32,
             mem: Memory::new(),
             curr_pc: 0u32,
+            csrs: Csrs::init(),
         }
     }
 
@@ -28,3 +55,4 @@ impl Cpu {
         }
     }
 }
+
