ec116f0b24459b10832388dccbc3fcf54172af1f
Om Barkare <ombarkare123@gmail.com>
2026-06-07 16:47:23 +0530

    src/fetcher.rs use curr_pc to fetch
    pc was being saved, updated and then used to fetch, which
    resulted in the fetch unit fetching instructions one instruction
    ahead of what was expected.
    using curr_pc solves the problem.

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -5,6 +5,9 @@ impl Cpu {
     pub fn fetch(&mut self) -> RawInstruction {
         self.curr_pc = self.pc;
         self.pc += 4;
-        RawInstruction { bits: self.mem.read_32(self.pc).unwrap() }
+        RawInstruction {
+            bits: self.mem.read_32(self.curr_pc).unwrap(),
+        }
     }
 }
+
