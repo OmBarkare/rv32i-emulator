4766768e0273022c8bc74752cb3bada1e95ae783
Om Barkare <ombarkare123@gmail.com>
2026-06-07 16:52:54 +0530

    src/main.rs refactor and change test-file path
    change test file path to run new binary.
    change print messages to be more readable.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ use std::{fs::File, io::Read};
 
 fn main() {
     let mut cpu = Cpu::new();
-    let mut file = File::open("/home/om/omomo/projects/rv32i-emulator/tests/binary").unwrap();
+    let mut file =
+        File::open("/home/om/omomo/projects/rv32i-emulator/tests/rv32i_zicsr.bin").unwrap();
     let mut binary: Vec<u8> = Vec::new();
 
     file.read_to_end(&mut binary).unwrap();
@@ -19,19 +20,19 @@ fn main() {
     loop {
         let raw_inst = cpu.fetch();
 
+        // Halt instruction is jal r0, 0
         if raw_inst.bits == 0x0000006F {
-            println!("HALT");
+            println!("HALT ENCOUNTERED");
             cpu.dump_registers();
             break;
         }
 
         println!("inst_hex: 0X{:X}", &raw_inst.bits);
+        println!("PC(next inst): 0X{:X}", cpu.pc);
         let inst = cpu.decode(raw_inst);
 
         println!("{:#?}", inst);
 
         cpu.execute(inst);
-
-        println!("PC: 0X{:X}", cpu.pc);
     }
 }
