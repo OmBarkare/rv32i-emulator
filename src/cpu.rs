b15ecc22c3a84ecfc3a588b8fdde3ddfb53df0cb
Om Barkare <ombarkare123@gmail.com>
2026-06-13 17:32:29 +0530

    src/cpu.rs add static elf loading functionality
    add a load_elf() function. Also changed the load_binary function
    to take in a starting address so that it can be used to copy the
    bytes in load_elf() function.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use crate::csrs::Csrs;
 use crate::memory::Memory;
 
@@ -20,10 +22,82 @@ impl Cpu {
         }
     }
 
-    pub fn load_binary(&mut self, binary: &[u8]) {
+    pub fn load_binary(&mut self, binary: &[u8], start_addr: u32) {
         for (i, byte) in binary.iter().enumerate() {
-            self.mem.write_8(i as u32, *byte).unwrap();
+            self.mem.write_8(i as u32 + start_addr, *byte).unwrap();
         }
+    }
+
+    pub fn load_elf(&mut self, elf: &[u8]) -> Result<(), String> {
+        if &elf[0..4] != b"\x7fELF" {
+            return Err(String::from("not an elf"));
+        }
+
+        // 4th is 1 for 32 bit address space
+        if elf[4] != 1 {
+            return Err(String::from("elf not for 32 bit address space"));
+        }
+
+        // 5th bit is 1 for little endian encoding
+        if elf[5] != 1 {
+            return Err(String::from("encoding not little endian"));
+        }
+        // 6th, 7th and 8th byte represent version,
+        // OS ABI, ABI version (almost never used). Not
+        // checking these because not super important
+        // next 7 bytes after that is padding
+
+        // e_machine
+        let e_machine: u16 = ((elf[19] as u16) << 8) | elf[18] as u16;
+        if e_machine != 0xF3 {
+            return Err(String::from("not for RISCV"));
+        }
+
+        // e_entry
+        let e_entry: u32 = (elf[27] as u32) << 24
+            | (elf[26] as u32) << 16
+            | (elf[25] as u32) << 8
+            | elf[24] as u32;
+        self.pc = e_entry;
+
+        let e_phoff: u32 = (elf[31] as u32) << 24
+            | (elf[30] as u32) << 16
+            | (elf[29] as u32) << 8
+            | elf[28] as u32;
+
+        let e_phentsize: u16 = (elf[43] as u16) << 8 | (elf[42] as u16);
+
+        let e_phnum: u16 = (elf[45] as u16) << 8 | (elf[44] as u16);
+
+        // reading the program headers and
+        // loading segments
+        for i in 0..e_phnum {
+            let start_addr: usize = (e_phoff + (e_phentsize as u32) * (i as u32)) as usize;
+            if elf[start_addr] != 1 {
+                continue;
+            }
+
+            let offset = (elf[start_addr + 7] as u32) << 24
+                | (elf[start_addr + 6] as u32) << 16
+                | (elf[start_addr + 5] as u32) << 8
+                | elf[start_addr + 4] as u32;
+
+            let vaddr = (elf[start_addr + 11] as u32) << 24
+                | (elf[start_addr + 10] as u32) << 16
+                | (elf[start_addr + 9] as u32) << 8
+                | elf[start_addr + 8] as u32;
+
+            let filesz = (elf[start_addr + 19] as u32) << 24
+                | (elf[start_addr + 18] as u32) << 16
+                | (elf[start_addr + 17] as u32) << 8
+                | elf[start_addr + 16] as u32;
+            // all memory is already initialized to zero
+            // so no need to read memsize
+
+            let bytes = &elf[(offset as usize)..((offset + filesz) as usize)];
+            self.load_binary(bytes, vaddr);
+        }
+        Ok(())
     }
 
     pub fn dump_registers(&self) {
