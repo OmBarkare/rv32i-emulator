ef270e2a009d2449e11295b096ac5e7a2306ba8b
Om Barkare <ombarkare123@gmail.com>
2026-06-16 15:58:16 +0530

    src/main change test file

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
         .init();
     let mut cpu = Cpu::new();
     let mut file =
-        File::open("/home/om/omomo/projects/rv32i-emulator/dump/basic_exec_cssrs.elf").unwrap();
+        File::open("/home/om/omomo/projects/rv32i-emulator/tests/print_example.elf").unwrap();
     let mut binary: Vec<u8> = Vec::new();
 
     file.read_to_end(&mut binary).unwrap();
@@ -31,22 +31,16 @@ fn main() {
 
         // Halt instruction is jal r0, 0
         if raw_inst.bits == 0x0000006F {
-            println!("HALT ENCOUNTERED");
+            log::debug!("HALT ENCOUNTERED");
             cpu.dump_registers();
-            println!("PRINTING MEMORY:\n");
-            println!(
-                "mem[0x44]: 0X{:X}\nmem[0x40]: 0X{:X}",
-                cpu.mem.read_32(0x44).unwrap(),
-                cpu.mem.read_32(0x40).unwrap()
-            );
             break;
         }
 
-        println!("inst_hex: 0X{:X}", &raw_inst.bits);
-        println!("PC: 0X{:X}", cpu.curr_pc);
+        log::debug!("inst_hex: 0X{:X}", &raw_inst.bits);
+        log::debug!("PC: 0X{:X}", cpu.curr_pc);
         let inst = cpu.decode(raw_inst);
 
-        println!("{:#?}\n\n", inst);
+        log::debug!("{:#?}\n\n", inst);
 
         cpu.execute(inst);
     }
